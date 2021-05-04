23680886b8d62ac97ba00ad29b190faa9bbaf887
Franek <kamieniecki.franek@gmail.com>
2021-05-04 17:22:52 +0200

    koniec projektu

--- notatnik/notatnik.js
+++ notatnik/notatnik.js
@@ -41,7 +41,7 @@ const createNote = () => {
     newNote.innerHTML = `
         <div class="note-header">
         <h3 class="note-title">${selectedValue}</h3>
-        <button class="delete-note" onclick="deleteNote(${cardID})">
+        <button class="delete-note"  onclick="deleteNote(${cardID})">
             <i class="fas fa-times icon"></i>
         </button>
         </div>
@@ -56,13 +56,37 @@ const createNote = () => {
     category.selectedIndex = 0;
     notePanel.style.display = 'none';
     checkColor(newNote)
+
 }
 
 const selectValue = () => {
     selectedValue = category.options[category.selectedIndex].text;
-    console.log(selectedValue)
+
+}
+
+const checkColor = note => {
+    switch (selectedValue) {
+        case 'Zakupy':
+            note.style.backgroundColor = 'rgb(72,255,0)';
+            break;
+        case 'Praca':
+            note.style.backgroundColor = 'rgb(255,243,0)';
+            break;
+        case 'Inne':
+            note.style.backgroundColor = 'rgb(0,170,255)';
+            break;
+    }
+}
+const deleteNote = id => {
+    const noteToDelete = document.getElementById(id);
+    noteArea.removeChild(noteToDelete)
+
+}
+const deleteAllNotes = () => {
+    noteArea.textContent = ''
 }
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
 saveBtn.addEventListener('click', addNote);
+deleteAllBtn.addEventListener('click', deleteAllNotes)
 
